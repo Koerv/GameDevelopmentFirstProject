8d65143be9724f127e1b8e79fc6d447a88df5d9d
boca87 <patrickmaul87@googlemail.com>
2016-10-15 16:25:05 +0300

    Turning Point changes

--- Assets/Scripts/TurningPoint.cs
+++ Assets/Scripts/TurningPoint.cs
@@ -4,10 +4,30 @@ using System.Collections.Generic;
 
 public class TurningPoint : MonoBehaviour {
 
-    public List<Vector3> directions;
+
+    public bool down, left_downward, right_downward, up, left_upward, right_upward;
+    List<Vector3> directions;
 
     public Vector3 newDirection()
     {
+        directions = new List<Vector3>();
+        if (GameManager.instance.wayDown) { 
+            if (down)
+                directions.Add(new Vector3(0, -1, 0));
+            if (left_downward)
+                directions.Add(new Vector3(-1, 0, 0));
+            if (right_downward)
+                directions.Add(new Vector3(1, 0, 0));
+        }
+        else
+        {
+            if(up)
+                directions.Add(new Vector3(0, 1, 0));
+            if(left_upward)
+                directions.Add(new Vector3(-1, 0, 0));
+            if(right_upward)
+                directions.Add(new Vector3(1, 0, 0));
+        }
         //TODO: Add Booleans to decide the walking direction?
         //return one random Value of List
         return directions[Random.Range(0, directions.Count)];
